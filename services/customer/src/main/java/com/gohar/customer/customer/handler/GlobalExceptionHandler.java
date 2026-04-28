c6252e4482de9e344acc0537c95ebccce3c6c078
Mohamed Younes <74791546+MohamedYounesElsaqqa@users.noreply.github.com>
2026-04-28 05:09:04 +0300

    add GlobalExceptionHandler for centralized error handling with custom exceptions and standardized response structure

--- services/customer/src/main/java/com/gohar/customer/customer/handler/GlobalExceptionHandler.java
+++ services/customer/src/main/java/com/gohar/customer/customer/handler/GlobalExceptionHandler.java
@@ -1,7 +1,30 @@
 package com.gohar.customer.customer.handler;
 
+import com.gohar.customer.customer.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> handle(CustomerNotFoundException customerNotFoundException){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(customerNotFoundException.getMsg());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException methodArgumentNotValidException){
+        var errors = new HashMap<String,String>();
+        methodArgumentNotValidException.getBindingResult().getAllErrors()
+                .forEach(error ->{
+                  var filedName = ((FieldError)error).getField();
+                  var errorMassage = error.getDefaultMessage();
+                  errors.put(filedName,errorMassage);
+                });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors));
+    }
 }
