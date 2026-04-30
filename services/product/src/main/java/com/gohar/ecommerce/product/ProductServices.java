9fa114eeae3ada93a75455d66c954b7eeab3b3de
Mohamed Younes <74791546+MohamedYounesElsaqqa@users.noreply.github.com>
2026-04-30 14:35:56 +0300

    create services and add flyway insert

--- services/product/src/main/java/com/gohar/ecommerce/product/ProductServices.java
+++ services/product/src/main/java/com/gohar/ecommerce/product/ProductServices.java
@@ -1,26 +1,60 @@
 package com.gohar.ecommerce.product;
 
+import com.gohar.ecommerce.exception.ProductPurchaseException;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServices {
-
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
     public ProductResponse findById(Integer productId) {
-        return null;
+        return productRepository.findById(productId)
+                .map(productMapper::toProductResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with the id " + productId));
     }
 
-    public List<ProductPurchaseResponse> pruchaseProducts(List<ProductPurchaseRequest> productPurchaseRequests) {
-        return List.of();
+    public List<ProductPurchaseResponse> purchaseProducts(List<ProductPurchaseRequest> productPurchaseRequests) {
+        var productIds = productPurchaseRequests.stream().map(ProductPurchaseRequest::productId)
+                .toList();
+        var storedProduct = productRepository.findAllByIdInOrderById(productIds);
+        if(productIds.size() != storedProduct.size()){
+            throw new ProductPurchaseException("One more product dose not exits");
+        }
+        var storedRequest = productPurchaseRequests
+                .stream()
+                .sorted(Comparator.comparing(ProductPurchaseRequest::productId))
+                .toList();
+        var purchasedProduct = new ArrayList<ProductPurchaseResponse>();
+        for (int i = 0; i < storedProduct.size(); i++ ){
+            var product = storedProduct.get(i);
+            var productRequest = storedRequest.get(i);
+            if(product.getAvailableQuantity() < productRequest.quantity()){
+                throw new ProductPurchaseException("Insufficient stock quantity for product with id : " + productRequest.productId());
+            }
+            var newAvailableQuantity = product.getAvailableQuantity() - productRequest.quantity();
+            product.setAvailableQuantity(newAvailableQuantity);
+            productRepository.save(product);
+            purchasedProduct.add(productMapper.toProductPurchaseResponse(product,productRequest.quantity()));
+        }
+        return purchasedProduct;
     }
 
     public Integer createProduct(@Valid ProductRequest productRequest) {
-        return 0;
+        var product = productMapper.toProduct(productRequest);
+        return productRepository.save(product).getId();
     }
 
     public List<ProductResponse> getAllProduct() {
-        return List.of();
+        return productRepository.findAll().stream().map(productMapper::toProductResponse)
+                .collect(Collectors.toList());
     }
 }
